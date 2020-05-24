cbb2133d755ba6ad1e6d009d5a148c1d7f43e7aa
whitw <shkim7876@naver.com>
2020-05-24 22:40:42 +0900

    diagnosis layout

--- pyclamd/server.py
+++ pyclamd/server.py
@@ -11,29 +11,33 @@ app.config['SECRET_KEY'] = SECRET_KEY
 cd = pyclamd.ClamdAgnostic()
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('main.html')
 
 @app.route('/diagnosis', methods=['GET', 'POST'])
 def diagnosis():
-    results=[]
+    s_results=[]
+    d_results=[]
     form = FileForm()
     if form.validate_on_submit():
         for testfile in form.testfile.data:
-            if(testfile):
-                tfname , tfext = os.path.splitext(testfile.filename)
-                storedname = os.path.realpath(os.path.curdir) + '/testdata/' + token_hex(8) + tfext
-                testfile.save(storedname)
-                result = cd.scan_file(storedname)
-                if result:
-                    try:
-                        result[testfile.filename] = result.pop(storedname)
-                    except Exception:
-                        assert(False)
-                    results.append(result)
-                os.remove(storedname)
-                del storedname
-        return render_template('result.html', results=results)
+            tfname, tfext = os.path.splitext(testfile.filename)
+            storedname = os.path.realpath(os.path.curdir) + '/testdata/' + token_hex(8) + tfext
+            testfile.save(storedname)
+            result = cd.scan_file(storedname)
+            if result:
+                try:
+                    result[testfile.filename] = result.pop(storedname)
+                except Exception:
+                    assert(False)
+                d_results.append(result)
+            else:
+                s_results.append({testfile.filename: ('SAFE', 'NO')})
+            os.remove(storedname)
+            del storedname
+        return render_template('result.html',
+            danger_results=d_results, safe_results=s_results)
     return render_template('diagnosis.html', form=form)
 
 @app.route('/wiki')
@@ -44,9 +48,6 @@ def wikiMain():
 @app.route('/wiki/w/<search>')
 def wikiIndex(search):
     return render_template('wiki.html', search=search)
-@app.route('/version')
-def version():
-    return render_template('version.html', cd=cd)
 
 if __name__ == '__main__':
     app.run(debug=True)
