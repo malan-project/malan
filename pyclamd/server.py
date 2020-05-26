0fae561248d5970a4137552124e71dbf716653ee
Byeonggon Lee <gonny952@gmail.com>
2020-05-27 01:41:25 +0900

    Modify pyclamd/server.py

--- pyclamd/server.py
+++ pyclamd/server.py
@@ -1,12 +1,23 @@
 from flask import Flask
 import pyclamd
+import os
 
 app = Flask(__name__)
-cd = pyclamd.ClamdAgnostic()
+#cd = pyclamd.ClamdAgnostic()
+cd = pyclamd.ClamdUnixSocket()
+
+if not cd.ping():
+  raise Exception('Unable to connect to clamd')
 
 @app.route('/version')
 def status():
-  if cd.ping():
     return cd.version()
-  else:
-    'Unable to connect to clamd'
+
+@app.route('/scan/<path>')
+def scan(path):
+    path = os.getcwd() + '/' + path
+    res = cd.scan_file(path)
+    if res is None:
+        return f'{path} is clean'
+    else:
+        return res
