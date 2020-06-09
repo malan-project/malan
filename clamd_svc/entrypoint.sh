8f90c8220b4a01e15c0328120d106660f2198320
Byeonggon Lee <gonny952@gmail.com>
2020-06-10 02:12:50 +0900

    Use docker-compose

--- clamd_svc/entrypoint.sh
+++ clamd_svc/entrypoint.sh
@@ -1,2 +1,2 @@
 /usr/sbin/clamd
-uwsgi --plugins-dir /usr/lib/uwsgi --need-plugin python3 --plugins-list --socket 0.0.0.0:5000 --protocol http --chdir /srv -w wsgi:app
+uwsgi --plugins-dir /usr/lib/uwsgi --need-plugin python3 --plugins-list --socket 0.0.0.0:8080 --protocol http -w srv.wsgi:app
--- clamd_svc/wsgi.py
+++ clamd_svc/wsgi.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     DEBUG = False
     if 'DEBUG' in os.environ and os.environ['DEBUG'] == '1':
         DEBUG = True
-        app.run(host='0.0.0.0', debug=DEBUG)
+    app.run(host='0.0.0.0', debug=DEBUG)
