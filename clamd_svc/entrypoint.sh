bd2f6cf1dae44a7454f3310bb6348bce6596479b
Byeonggon Lee <gonny952@gmail.com>
2020-06-18 06:32:42 +0900

    Fix entrypoint.sh

--- clamd_svc/entrypoint.sh
+++ clamd_svc/entrypoint.sh
@@ -1,2 +1,2 @@
 /usr/sbin/clamd
-uwsgi --master --plugins-dir /usr/lib/uwsgi --need-plugin python3 --socket 0.0.0.0:8080 --protocol http -w srv.wsgi:app
+uwsgi --master --plugins-dir /usr/lib/uwsgi/plugins --need-plugin python3 --socket 0.0.0.0:8080 --protocol http -w srv.wsgi:app
